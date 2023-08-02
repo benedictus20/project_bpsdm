d3de4b0f83c9341a374fdd1fd34c357059713ce7
benedictus20 <benisihotang17@gmail.com>
2023-08-02 14:05:02 +0700

    update database terbaru

--- system/database/agenda_bpsdm.sql
+++ system/database/agenda_bpsdm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 31, 2023 at 05:30 AM
+-- Generation Time: Aug 02, 2023 at 09:03 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 7.4.25
 
@@ -20,6 +20,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `agenda_bpsdm`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `agenda_bidang`
+--
+
+CREATE TABLE `agenda_bidang` (
+  `id` int(10) NOT NULL,
+  `tgl` date NOT NULL,
+  `nama_kegiatan` varchar(225) NOT NULL,
+  `bidang_penyelenggara` varchar(50) NOT NULL,
+  `Jam` time NOT NULL,
+  `penyelenggara` varchar(200) NOT NULL,
+  `tempat_kegiatan` varchar(225) NOT NULL,
+  `buka_acara` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `agenda_bidang`
+--
+
+INSERT INTO `agenda_bidang` (`id`, `tgl`, `nama_kegiatan`, `bidang_penyelenggara`, `Jam`, `penyelenggara`, `tempat_kegiatan`, `buka_acara`) VALUES
+(1, '2023-08-09', 'Pelatihan', 'sekretariat', '09:00:00', 'BPSDM Provinsi Jawa Barat', 'Gedung A', 'ACEP BAMBANG MUTAKIN,S.STP., M.Si.'),
+(2, '2023-08-03', 'Webinar', 'Bidang SKPK', '09:00:00', 'BPSDM Provinsi Jawa Barat', 'Gedung B', 'ANGGA MUCHLIS AL-RACHMAT,S.I.P., M.Si.');
 
 -- --------------------------------------------------------
 
@@ -42,6 +67,12 @@ INSERT INTO `user` (`username`, `password`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `agenda_bidang`
+--
+ALTER TABLE `agenda_bidang`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
